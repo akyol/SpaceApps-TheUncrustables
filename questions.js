5215cc6b78605963cd447a7152e47b2758f69428
Abraham Lee <abelee2016@gmail.com>
2019-10-19 14:11:12 -0700

    add questions dataset

---
+++ questions.js
@@ -0,0 +1,74 @@
+var questions = {
+    1: {
+        a: {
+            message: "",
+            correct: true
+        },
+        b: {
+            message: "",
+            correct: true
+        },
+        c: {
+            message: "",
+            correct: true
+        },
+        d: {
+            message: "",
+            correct: true
+        },
+    },
+    2: {
+        a: {
+            message: "",
+            correct: true
+        },
+        b: {
+            message: "",
+            correct: true
+        },
+        c: {
+            message: "",
+            correct: true
+        },
+        d: {
+            message: "",
+            correct: true
+        },
+    },
+    3: {
+        a: {
+            message: "",
+            correct: true
+        },
+        b: {
+            message: "",
+            correct: true
+        },
+        c: {
+            message: "",
+            correct: true
+        },
+        d: {
+            message: "",
+            correct: true
+        }
+    }
+                4: {
+        a: {
+            message: "",
+            correct: true
+        },
+        b: {
+            message: "",
+            correct: true
+        },
+        c: {
+            message: "",
+            correct: true
+        },
+        d: {
+            message: "",
+            correct: true
+        }
+    }
+};
